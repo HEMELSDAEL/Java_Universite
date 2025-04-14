9abc3bd1b00e26d0c2d6c95ab3b3f16c6e961449
HEMELSDAEL Alexandre <alexandre.hemelsdael.etu@univ-lille.fr>
2025-04-14 13:57:28 +0200

    suppression de la javadoc

--- tp5/docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"battleship"},{"l":"battleship.util"}];updateSearchResults();
--- tp6/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"image.util","l":"ImageDisplayer"},{"p":"image","l":"ImageInterface"},{"p":"image.util","l":"ImageLoader"}]
